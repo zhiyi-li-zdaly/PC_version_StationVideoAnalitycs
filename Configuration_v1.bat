68e1970e3eeeb5e3044e498744111444a0dc204c
zhiyi-li-zdaly <126203059+zhiyi-li-zdaly@users.noreply.github.com>
2023-09-08 09:07:13 -0500

    Add files via upload

---
+++ Configuration_v1.bat
@@ -0,0 +1,7 @@
+:: Step 0. Goto the directory
+cd C:\Users\zhiyi\Downloads\Jetson_Nano_Video_Analysis\TestResultsOn20230905\pc_version_StationVideoAnalitycs
+mkdir samples
+
+:: Step 1. Show the camera's view and let user draw the line
+python captureCameraViewDrawLineRecordCoordinates_v2.py
+PAUSE
