df556e26210adf103d48f04f43c7965dcabe1994
zhiyi-li-zdaly <126203059+zhiyi-li-zdaly@users.noreply.github.com>
2023-09-11 15:28:04 -0500

    Add files via upload

--- run_pc_version_StationVideoAnalytics_v1.bat
+++ run_pc_version_StationVideoAnalytics_v1.bat
@@ -1,3 +1,7 @@
-:: Step 1. Run program
+
+:: Step 0: Launch Anaconda environment from bat
+call C:\Users\zhiyi\anaconda3\condabin\_conda_activate.bat
+pip install -r requirements.txt
+
 python main_v7.py
 PAUSE
